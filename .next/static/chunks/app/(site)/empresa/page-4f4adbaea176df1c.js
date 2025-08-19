4af75fc26799d5e95ecc607edc218d67cb11a5fb
Gonapertech <gaguirre@aperturetechnologies.es>
2025-08-19 15:38:13 +0200

    Web Compila

---
+++ .next/static/chunks/app/(site)/empresa/page-4f4adbaea176df1c.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[189],{8756:(e,s,n)=>{Promise.resolve().then(n.bind(n,4777)),Promise.resolve().then(n.t.bind(n,6874,23))}},e=>{e.O(0,[874,444,777,441,964,358],()=>e(e.s=8756)),_N_E=e.O()}]);
